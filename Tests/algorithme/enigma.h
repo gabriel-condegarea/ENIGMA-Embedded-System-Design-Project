8a5d3a91771f0ed91a7c003e6623a2f1bae2d95a
Andrea Sisto <sisto.andrea@reno-cardan.ch>
2026-03-30 11:14:48 +0200

    Start moving enigma functions to another .c file, currently an error

--- Tests/algorithme/enigma.h
+++ Tests/algorithme/enigma.h
@@ -21,3 +21,13 @@ struct Enigma {
     const char      *reflector;
     struct Rotor    rotors[8];
 };
+
+
+//global vars
+extern const char *alpha;
+extern const char *rotor_ciphers[];
+extern const char *rotor_notches[];
+extern const char *rotor_turnovers[];
+extern const char *reflectors[];
+extern const char *rotor_names[];
+extern const char *plugboard;
